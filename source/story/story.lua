81457fd726aac71a91f395da7e85c9cfac6c4d5c
Mike B <mikebalvert@icloud.com>
2024-03-24 23:37:05 +0100

    made it so what when you hold the atention thing will be shown

--- source/story/story.lua
+++ source/story/story.lua
@@ -107,7 +107,13 @@ function story.npc:draw()
             love.graphics.setColor(0,1,1)
         end
         if story.data.storyTold.john1 == false then
+            love.graphics.stencil(function()
+                love.graphics.rectangle("fill", story.npc.john.x + 5 - story.dialogue.storyAvalible:getWidth() / 2, (story.npc.john.y - 20) - story.dialogue.storyAvalible:getHeight(), story.npc.interactionHold * story.dialogue.storyAvalible:getHeight(), story.dialogue.storyAvalible:getHeight())
+            end, "replace", 1)
+            
+            love.graphics.setStencilTest("greater", 0)
             love.graphics.draw(story.dialogue.storyAvalible, story.npc.john.x + 5 - story.dialogue.storyAvalible:getWidth() / 2, (story.npc.john.y - 20) - story.dialogue.storyAvalible:getHeight())
+            love.graphics.setStencilTest()
         end
         love.graphics.setColor(1,1,1)
     end
