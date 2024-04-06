6b7a0a895b8b220243efc36057c979a1dc5ad29a
mike b <mikebalvert@icloud.com>
2024-04-06 12:40:52 +0200

    buttons are perfect now (added hover color)

--- source/screens/button.lua
+++ source/screens/button.lua
@@ -16,6 +16,7 @@ function button.new(x, y, text, color, id)
     self.imageOutline = buttonImageOutline
     self.color = color
     self.text = text
+    self.hover = false
     return self
 end
 
@@ -37,19 +38,23 @@ function button:update()
     local x = love.mouse.getX()
     local y = love.mouse.getY()
     if x > love.graphics.getWidth() / 2 + (self.x * playerCamera.globalScale) and x < love.graphics.getWidth() / 2 + ((self.x + self.width) * playerCamera.globalScale) and y > love.graphics.getHeight() / 2 + (self.y * playerCamera.globalScale) and y < love.graphics.getHeight() / 2 + ((self.y + self.height) * playerCamera.globalScale) then
-        --hover
+        self.hover = true
         if love.mouse.isDown(1) then
             print("Button clicked")
             action(self.id)
         end
     else
-        --normal
+        self.hover = false
     end
 end
 
 function button:draw(image, x, y)
     love.graphics.draw(self.image, love.graphics.getWidth() / 2 + (x * playerCamera.globalScale), love.graphics.getHeight() / 2 + (y * playerCamera.globalScale) , nil, playerCamera.globalScale)
-    love.graphics.setColor(self.color[1], self.color[2], self.color[3])
+    if self.hover then
+        love.graphics.setColor(self.color[1], self.color[2], self.color[3])
+    else
+        love.graphics.setColor(0.15, 0.15, 0.15)
+    end
     love.graphics.draw(self.imageOutline, love.graphics.getWidth() / 2 + (x * playerCamera.globalScale), love.graphics.getHeight() / 2 + (y * playerCamera.globalScale) , nil, playerCamera.globalScale)
     love.graphics.print(self.text, love.graphics.getWidth() / 2 + ((x + 40) * playerCamera.globalScale) - (font:getWidth(self.text) * playerCamera.globalScale) / 2, love.graphics.getHeight() / 2 + ((y + 10) * playerCamera.globalScale) - (font:getHeight(self.text) * playerCamera.globalScale) / 2, nil, playerCamera.globalScale)
     love.graphics.setColor(1, 1, 1)
