8ab0e88072125218658e1ee85004b8d149e59738
Mike Balvert <mikebalvert@icloud.com>
2024-05-18 19:29:48 +0200

    changed lua version in conf

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
 	t.window.title = "Ruined | Title Screen"
     t.window.icon = "assets/textures/gui/title/logo1.png"
-    t.version = 11.4
+    t.version = 11.5
     t.window.highdpi = true
     t.window.resizable = true
     t.console = false
