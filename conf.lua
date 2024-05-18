dada5ff498fa90607b3ae3283b8b5a1b572d00cb
Mike Balvert <mikebalvert@icloud.com>
2024-05-18 19:05:41 +0200

    removed old logo image and redirected the start window logo

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
 	t.window.title = "Ruined | Title Screen"
-    t.window.icon = "assets/textures/logo.png"
+    t.window.icon = "assets/textures/gui/title/logo1.png"
     t.version = 11.4
     t.window.highdpi = true
     t.window.resizable = true
