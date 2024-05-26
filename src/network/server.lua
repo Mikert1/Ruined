c5bc1d9aeedfb2fdb9bb8351524b1082a3125394
M Balvert <mikebalvert@icloud.com>
2024-05-26 11:50:04 +0200

    first take on connecting to LAN for multiplayer

--- src/network/server.lua
+++ src/network/server.lua
@@ -1,38 +1,26 @@
--- require the socket library
 local socket = require("socket")
+local ip = "192.168.0.100"  -- IP-adres van de host
+local port = 12345  -- Kies een poortnummer
 
--- create a UDP socket
 local udp = socket.udp()
+udp:settimeout(0)
+udp:setpeername(ip, port)
 
--- set local IP address and port
-local localIP = "127.0.0.1" -- het lokale IP-adres van de speler
-local localPort = 12345 -- het lokale poortnummer van de speler
-
--- bind the socket to the local address and port
-udp:setsockname(localIP, localPort)
-
--- set the IP address and port of the other player
-local otherPlayerIP = "192.168.1.100" -- het IP-adres van de andere speler
-local otherPlayerPort = 54321 -- het poortnummer van de andere speler
-
--- Definieer de server module
 local server = {}
 
 -- function to send data to the other player
 function server.sendData(data)
-    udp:sendto(data, otherPlayerIP, otherPlayerPort)
+    print("[Server] Sending data to other player: " .. data)
+    udp:send("Hello from Game 1!")
 end
 
 -- function to receive data from the other player
 function server.receiveData()
-    udp:settimeout(0) -- Stel de timeout in op 0 om niet-blokkerend te zijn
-    local data, msg = udp:receivefrom()
+    local data, msg = udp:receive()
     if data then
-        print("[Server] Received data from other player: " .. data)
-    elseif msg ~= "timeout" then
-        print("[Server] Error receiving data: " .. msg)
-    else
-        print("[Server] No data received")
+        print("Received: " .. data)
+    elseif msg ~= 'timeout' then
+        error("Network error: " .. tostring(msg))
     end
 end
 
