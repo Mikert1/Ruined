840f27763fa210aad8c1ad089024cb229ef3c720
M Balvert <mikebalvert@icloud.com>
2024-07-08 11:34:04 +0200

    fixed a window not responding bug

--- main.lua
+++ main.lua
@@ -56,6 +56,7 @@ print("[Debug ] Console is active\n    - use Tab to show more info and show coll
 
 function love.load()
     story.load()
+    objectsManager.load()
 end
 
 function love.update(dt)
--- src/gameplay/objects.lua
+++ src/gameplay/objects.lua
@@ -1,34 +1,36 @@
 local objectsManager = {}
 
-objectsManager.objects = {
-    tent = {
-        type = "static",
-        world = "Snow",
-        x = 167,
-        y = 286,
-        width = 46,
-        height = 61,
-        image = love.graphics.newImage("assets/textures/world/structures/tent.png"),
-        imageBackground = love.graphics.newImage("assets/textures/world/structures/tentBackground.png")
-    },
-    cryonium = {
-        type = "animation",
-        world = "Snow",
-        x = 0,
-        y = 0,
-        width = 13,
-        height = 18,
-        image = {},
-        animation = {
-            frame = 1,
-            timer = 0,
-            order = {1, 2}
+function objectsManager.load()
+    objectsManager.objects = {
+        tent = {
+            type = "static",
+            world = "Snow",
+            x = 167,
+            y = 286,
+            width = 46,
+            height = 61,
+            image = love.graphics.newImage("assets/textures/world/structures/tent.png"),
+            imageBackground = love.graphics.newImage("assets/textures/world/structures/tentBackground.png")
+        },
+        cryonium = {
+            type = "animation",
+            world = "Mountains",
+            x = 30,
+            y = 100,
+            width = 15,
+            height = 20,
+            image = {},
+            animation = {
+                frame = 1,
+                timer = 0,
+                order = {1, 2}
+            }
         }
     }
-}
 
-for i = 1, 2 do
-    objectsManager.objects.cryonium.image[i] = love.graphics.newImage("assets/textures/world/cryonium/a" .. i .. ".png")
+    for i = 1, 2 do
+        objectsManager.objects.cryonium.image[i] = love.graphics.newImage("assets/textures/world/cryonium/a" .. i .. ".png")
+    end
 end
 
 function objectsManager.update(dt)
@@ -47,8 +49,6 @@ function objectsManager.update(dt)
 end
 
 function objectsManager.draw(drawLayer, world)
-    print("Drawing objects")
-    print(objectsManager.objects)
     for k, v in pairs(objectsManager.objects) do
         if v.world == world then
             if v.type == "static" then
@@ -58,6 +58,7 @@ function objectsManager.draw(drawLayer, world)
                     love.graphics.draw(v.image, v.x, v.y, 0, 1, 1, v.width / 2, v.height / 2)
                 end
             elseif v.type == "animation" then
+                love.graphics.rectangle("line", v.x - v.width / 2, v.y - v.height / 2, v.width, v.height)
                 if drawLayer == 1 then
                     love.graphics.draw(v.image[v.animation.order[v.animation.frame]], v.x, v.y, 0, 1, 1, v.width / 2, v.height / 2)
                 elseif drawLayer == 2 then
